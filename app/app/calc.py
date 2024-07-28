fac4276aaa6f8c99d888834e028c8ad0ba97e4b3
kkhaddejah <khadijahkotb17@gmail.com>
2024-07-28 20:50:54 +0400

    Fixed linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,8 @@ Calculator functions
 def add(x, y):
     """Add x and y and return resut."""
     return x + y
+
+
+def subtract(x, y):
+    """Subtract x and y and return resut."""
+    return y - x
