5d7f3cabb4104f2736c5724d65559e29c064f1fd
kkhaddejah <khadijahkotb17@gmail.com>
2024-07-29 20:10:53 +0400

    Adding wait_for_db command(updated, not complete)

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,10 +4,10 @@ Calculator functions
 
 
 def add(x, y):
-    """Add x and y and return resut."""
+    """Add x and y and return result."""
     return x + y
 
 
 def subtract(x, y):
-    """Subtract x and y and return resut."""
+    """Subtract x from y and return result."""
     return y - x
