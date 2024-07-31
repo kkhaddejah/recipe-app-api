b1164033dba3b80ea112b84287e9d8ef2bbde897
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2024-07-31 23:33:50 +0400

    Creating superusers(fixed).

--- app/core/models.py
+++ app/core/models.py
@@ -1,13 +1,13 @@
 """
 Database models.
 """
-from typing import Any
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -23,7 +23,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """Create and retrurn a new superuser"""
+        """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
