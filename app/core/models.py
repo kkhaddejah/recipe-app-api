1adbf120b5655794cb21b9b9c5e1c17ae3451c81
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2024-07-31 21:44:08 +0400

    flake8 errors fixed

--- app/core/models.py
+++ app/core/models.py
@@ -1,13 +1,13 @@
 """
 Database models.
 """
-from typing import Any
 from django.db import models
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,5 @@
 """
-Test for models.
+Tests for models.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -7,7 +7,6 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
     """Test models."""
-
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
