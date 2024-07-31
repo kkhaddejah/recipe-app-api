2750387ab88790fcd5f4250469eace5726bd47a1
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2024-07-31 23:30:27 +0400

    Creating superusers.

--- app/core/models.py
+++ app/core/models.py
@@ -1,13 +1,13 @@
 """
 Database models.
 """
+from typing import Any
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
-
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -18,6 +18,15 @@ class UserManager(BaseUserManager):
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and retrurn a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
