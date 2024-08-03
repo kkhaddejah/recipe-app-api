2a1df0876425c00f839de043fc23e27910d5432a
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2024-08-04 00:23:17 +0400

    Implemented recipe listing API.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,3 +1,15 @@
 """
-Serializers for the recipe app.
+Serializers for recipe app.
 """
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RcipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only = ['id']
